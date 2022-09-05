8757e9b0f879ad104cf2d057ba823a657b14bd1f
Tatiana <trubina-tanya@inbox.ru>
2022-09-05 18:33:37 +0300

    Example 001 Hello Console Project

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example001_HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
